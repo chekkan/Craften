cda33df9fa342a467db0695ac916f9421e75492f
Harish Babu <harish_babu@live.co.uk>
2012-11-21 20:41:46 +0000

    static function to convert an array to object

--- ArrayMethods.php
+++ ArrayMethods.php
@@ -25,6 +25,25 @@ namespace Craften
             // do nothing
         }
         
+        public static function toObject($array)
+        {
+            $result = new \stdClass();
+            
+            foreach ($array as $key => $value)
+            {
+                if (is_array($value))
+                {
+                    $result->{$key} = self::toObject($value);
+                }
+                else
+                {
+                    $result->{$key} = $value;
+                }
+            }
+            
+            return $result;
+        }
+        
         public static function clean($array)
         {
             return array_filter($array, function($item) {
